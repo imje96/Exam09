ad071b8502cd36012202ecb9ac04cc95db215099
imje96 <127702320+imje96@users.noreply.github.com>
2023-03-14 15:49:31 +0900

    Update NewCleric.java

--- NewCleric.java
+++ NewCleric.java
@@ -8,20 +8,19 @@ public class NewCleric {
     static final int MP_MAX = 10;
     int hp = HP_MAX; 
     int mp = MP_MAX;
-    // 기본적으로 갖고있는 속성(필드값)
-    // maxHP와 HP를 같이 수정할 수 있음 
+
     Random rand = new Random();
-    // Random을 Cleric에 빼는 것이 더 좋음
+    // 연습문제 9-2. 이 클래스는 new Cleric(“아서스", 40, 5) 와 같이, 이름, HP, MP 를 지정하여 인스턴스화 할 수 있다
     NewCleric(String name, int hp, int mp){
          this.name = name;   
          this.hp = hp;
          this.mp = mp; 
     } 
-    
+     // 연습문제 9-2. 이 클래스는 new Cleric(“아서스", 35) 와 같이, 이름과 HP만으로 지정하여 인스턴스화 할 수 있다. 이 때, MP는 최대 MP와 같은 값이 초기화 된다
     NewCleric(String name, int hp) {
         this(name, hp, MP_MAX);
    }
-    
+    // 연습문제 9-2. 이 클래스는 new Cleric(“아서스") 와 같이 이름만을 지정하여 인스턴스화 할 수 있다. 이 때, HP 와 MP 는 최대 HP와 최대 MP로 초기화 된다
     NewCleric(String name) {
         this(name, HP_MAX, MP_MAX);   
    }

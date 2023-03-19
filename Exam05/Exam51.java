94dc6a9144d5906d236afafbdfcba106cc49d21c
imje96 <127702320+imje96@users.noreply.github.com>
2023-03-19 23:05:59 +0900

    연습문제5_1

---
+++ Exam05/Exam51.java
@@ -0,0 +1,17 @@
+package Exam5;
+
+public class Exam51 {
+    public static void main(String[] args) {
+        introduceOneself();
+    }
+
+    public static void introduceOneself() {
+        String name = "김둘리";
+        int age = 300;
+				double height = 150.5;
+				String gender = "male";
+                
+        System.out.println(
+                "이름은 " + name + " 나이는 " + age + "이고" +" 키는 " + height + " 성별은 " + gender + "입니다.");
+    }
+}

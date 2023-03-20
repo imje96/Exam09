cc103b5efce2668c7f527c6ea460d86e8f6063cf
imje96 <127702320+imje96@users.noreply.github.com>
2023-03-20 21:55:44 +0900

    연습문제15_2

---
+++ Exam15/Exam152.java
@@ -0,0 +1,16 @@
+package Exam15;
+
+public class Exam152 {
+    public static void main(String[] args) throws NullPointerException {
+        String s = null;
+        try {
+            s.length();
+            System.out.println(s.length());
+        } catch (NullPointerException e) {
+            System.out.println("NullPointerException 예외를 catch 하였습니다");
+            System.out.println("---- stack trace (여기부터) ----");
+            e.printStackTrace();
+            System.out.println("---- stack trace (여기부터) ----");
+        }
+    }
+}

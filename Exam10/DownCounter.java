9a05155783b671c4be25fa3fb843c1bb248f72f8
imje96 <127702320+imje96@users.noreply.github.com>
2023-03-21 23:50:27 +0900

    연습문제10_2

---
+++ Exam10/DownCounter.java
@@ -0,0 +1,17 @@
+package Exam10;
+
+public class DownCounter {
+    private int count = 0;
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public void count() {
+        count--;
+    }
+}

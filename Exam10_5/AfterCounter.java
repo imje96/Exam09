34b88fe6b772978d2c047a2f9feff605a51dc51c
imje96 <127702320+imje96@users.noreply.github.com>
2023-03-21 23:52:18 +0900

    연습문제10_5

---
+++ Exam10_5/AfterCounter.java
@@ -0,0 +1,21 @@
+package Exam10_5;
+
+public class AfterCounter implements Counter {
+    private int count = 0;
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+    
+    @Override
+    public int count() {
+        count++;
+        return count;
+    }
+
+    
+}
---
+++ Exam10_5/Counter.java
@@ -0,0 +1,5 @@
+package Exam10_5;
+
+public interface Counter {
+    public abstract int count();
+}
